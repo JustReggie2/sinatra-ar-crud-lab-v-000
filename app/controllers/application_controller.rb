6dba50052608cfe23e2a2c91d4c20cc8cae4b3ff
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-12-17 02:51:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
     article = Article.new({title: params[:title], content: params[:content]})
     article.save
 
-    redirect '/articles'
+    erb :index
   end
 
   get '/articles' do
